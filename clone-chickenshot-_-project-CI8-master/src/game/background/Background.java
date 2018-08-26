60672ba19cd12fb598212115d345a9ac88ea01c0
BanhTuanAnh <anh98bnb@gmail.com>
2018-08-26 23:19:44 +0700

    add image

--- clone-chickenshot-_-project-CI8-master/src/game/background/Background.java
+++ clone-chickenshot-_-project-CI8-master/src/game/background/Background.java
@@ -1,6 +1,7 @@
 package game.background;
 
 import base.GameObject;
+import renderer.BackgroundRenderer;
 import renderer.ImageRenderer;
 import renderer.RectRenderer;
 
@@ -8,8 +9,8 @@ import java.awt.*;
 
 public class Background extends GameObject {
     public Background() {
-        this.renderer = new ImageRenderer("clone-chickenshot-_-project-CI8-master/image/8320.jpg", 1024, 600);
-        this.position.set(0,0);
-       // this.renderer = new RectRenderer(Color.BLACK,1024,600);
+       this.position.set(0,0);
+       this.renderer = new BackgroundRenderer("clone-chickenshot-_-project-CI8-master/image/8320.jpg", 1024, 600);
+        // this.renderer = new RectRenderer(Color.BLACK,1024,600);
     }
 }
